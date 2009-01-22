015284f16eb9134e9a73558e3b0500088d78761f
cjh84archive <cjh84archive@74e265c4-e898-11dd-b2d9-215335d0b316>
2009-01-22 16:49:02 +0000

    Added code

---
+++ code/train/train.py
@@ -0,0 +1,17 @@
+#import ann.bpnn
+
+class Trainer:
+
+	def load(self):
+		#for all files in TrainingData
+		#for all files in Accelerate
+		Accelerate001 = []
+		file = open('/windows/project/testing/TrainingData/Accelerate/001a.csv', 'r')
+		for line in file:
+			if line:
+			
+	
+	
+if __name__ == '__main__':
+	trainer = Trainer()
+	trainer.load()
